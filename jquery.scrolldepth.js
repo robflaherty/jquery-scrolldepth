9b497c26b2a7ce64761b46463d58b248aaf0a8f7
beghbali <bashir@gmail.com>
2012-09-06 14:41:40 -0700

    1)Added custom handler support 2) expanded element selectors

--- jquery.scrolldepth.js
+++ jquery.scrolldepth.js
@@ -4,128 +4,146 @@
  * Licensed under the MIT and GPL licenses.
  */
 ;(function ( $, window, document, undefined ) {
-  
-  "use strict";
 
-  var defaults = {
-    elements: [],
-    minHeight: 0,
-    offset: 0, // Not used yet
-    percentage: true,
-    testing: false
-  },
+    "use strict";
 
-  $window = $(window),
-  cache = [];
+    var defaults = {
+            elements: [],
+            minHeight: 0,
+            offset: 0, // Not used yet
+            percentage: true,
+            testing: false,
+            eventHandler: null
+        },
 
-  /*
-   * Plugin
-   */
-
-  $.scrollDepth = function(options) {
-    
-    var startTime = +new Date;
-
-    options = $.extend({}, defaults, options);
-
-    // Return early if document height is too small
-    if ( $(document).height() < options.minHeight ) {
-      return;
-    }
-
-    // Establish baseline (0% scroll)
-    sendEvent('Percentage', 'Baseline');
+        $window = $(window),
+        cache = [];
 
     /*
-     * Functions
+     * Plugin
      */
 
-    function sendEvent(action, label, timing) {
-      if (!options.testing) {
+    $.scrollDepth = function(options) {
 
-        _gaq.push(['_trackEvent', 'Scroll Depth', action, label, 1, true]);
+        var startTime = +new Date;
 
-        if (arguments.length > 2) {
-          _gaq.push(['_trackTiming', 'Scroll Depth', action, timing, label, 100]);
+        options = $.extend({}, defaults, options);
+
+        // Return early if document height is too small
+        if ( $(document).height() < options.minHeight ) {
+            return;
         }
 
-      } else {
-        $('#console').html(action + ': ' + label);
-      }
-    }
+        // Establish baseline (0% scroll)
+        sendEvent('Percentage', 'Baseline');
 
-    function calculateMarks(docHeight) {
-      return {
-        '25%' : parseInt(docHeight * 0.25, 10),
-        '50%' : parseInt(docHeight * 0.50, 10),
-        '75%' : parseInt(docHeight * 0.75, 10),
-        // 1px cushion to trigger 100% event in iOS
-        '100%': docHeight - 1
-      };
-    }
+        /*
+         * Functions
+         */
 
-    function checkMarks(marks, scrollDistance, timing) {
-      // Check each active mark
-      $.each(marks, function(key, val) {
-        if ( $.inArray(key, cache) === -1 && scrollDistance >= val ) {
-          sendEvent('Percentage', key, timing);
-          cache.push(key);
+        function googleAnalyticsHandler(action, label, timing) {
+            _gaq.push(['_trackEvent', 'Scroll Depth', action, label, 1, true]);
+            if (arguments.length > 2) {
+                _gaq.push(['_trackTiming', 'Scroll Depth', action, timing, label, 100]);
+            }
         }
-      });
-    }
 
-    function checkElements(elements, scrollDistance, timing) {
-      $.each(elements, function(index, elem) {
-        if ( $.inArray(elem, cache) === -1 && $(elem).length ) {
-          if ( scrollDistance >= $(elem).offset().top ) {
-            sendEvent('Elements', elem, timing);
-            cache.push(elem);
-          }
+        function sendEvent(action, label, timing) {
+            if (!options.testing) {
+                if (options.eventHandler) {
+                    options.eventHandler(action, label, timing);
+                } else {
+                    googleAnalyticsHandler(action, label, timing);
+                }
+            } else {
+                $('#console').html(action + ': ' + label);
+            }
         }
-      });
-    }
 
-    /*
-     * Scroll Event
-     */
+        function calculateMarks(docHeight) {
+            return {
+                '25%' : parseInt(docHeight * 0.25, 10),
+                '50%' : parseInt(docHeight * 0.50, 10),
+                '75%' : parseInt(docHeight * 0.75, 10),
+                // 1px cushion to trigger 100% event in iOS
+                '100%': docHeight - 1
+            };
+        }
 
-    $window.on('scroll.scrollDepth', function() {
+        function checkMarks(marks, scrollDistance, timing) {
+            // Check each active mark
+            $.each(marks, function(key, val) {
+                if ( $.inArray(key, cache) === -1 && scrollDistance >= val ) {
+                    sendEvent('Percentage', key, timing);
+                    cache.push(key);
+                }
+            });
+        }
 
-      /*
-       * We calculate document and window height on each scroll event to
-       * account for dynamic DOM changes.
-       */
+        function checkElements(elements, scrollDistance, timing) {
+            var label;
+            $.each(elements, function(index, elem) {
+                if ( $.inArray(elem, cache) === -1 && $(elem).length ) {
+                    if ( scrollDistance >= $(elem).offset().top ) {
+                        label = (typeof elem.id == "undefined") ? elem.localName : elem.id;
+                        sendEvent('Elements', label, timing);
+                        cache.push(elem);
+                    }
+                }
+            });
+        }
 
-      var docHeight = $(document).height(),
-        winHeight = window.innerHeight ? window.innerHeight : $window.height(),
-        scrollDistance = $window.scrollTop() + winHeight,
+        /*
+         * Scroll Event
+         */
 
-        // Offset not being used yet
-        offset = parseInt(winHeight * (options.offset / 100), 10),
+        $window.on('scroll.scrollDepth', function() {
 
-        // Recalculate percentage marks
-        marks = calculateMarks(docHeight),
+            /*
+             * We calculate document and window height on each scroll event to
+             * account for dynamic DOM changes.
+             */
 
-        // Timing
-        timing = +new Date - startTime;
+            var docHeight = $(document).height(),
+                winHeight = window.innerHeight ? window.innerHeight : $window.height(),
+                scrollDistance = $window.scrollTop() + winHeight,
 
-      // If all marks already hit, unbind scroll event
-      if (cache.length >= 4 + options.elements.length) {
-        $window.off('scroll.scrollDepth');
-        return;
-      }
+            // Offset not being used yet
+                offset = parseInt(winHeight * (options.offset / 100), 10),
 
-      // Check specified DOM elements
-      if (options.elements) {
-        checkElements(options.elements, scrollDistance, timing);
-      }
+            // Recalculate percentage marks
+                marks = calculateMarks(docHeight),
 
-      // Check standard marks
-      if (options.percentage) {        
-        checkMarks(marks, scrollDistance, timing);
-      }
-    });
+            // Timing
+                timing = +new Date - startTime;
 
-  };
+            // If all marks already hit, unbind scroll event
+            var elements = expand(options.elements);
+
+            if (cache.length >= 4 + elements.length) {
+                $window.off('scroll.scrollDepth');
+                return;
+            }
+
+            // Check specified DOM elements
+            if (options.elements) {
+                checkElements(elements, scrollDistance, timing);
+            }
+
+            // Check standard marks
+            if (options.percentage) {
+                checkMarks(marks, scrollDistance, timing);
+            }
+        });
+
+        function expand(element_selectors) {
+            var elements = [];
+            $.each(element_selectors, function(i, selector) {
+                $.merge(elements, $(selector));
+            })
+            return elements;
+        }
+
+    };
 
 })( jQuery, window, document );
