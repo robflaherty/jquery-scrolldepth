b976669df54710db20baacb24b5193619b226c77
Andrea Cognini <info@heavybeard.it>
2015-12-28 21:13:22 +0100

    add custom marks on index test and remove them from script

--- jquery.scrolldepth.js
+++ jquery.scrolldepth.js
@@ -34,9 +34,7 @@
       gaGlobal: false,
       gtmOverride: false,
       markGap: 25,
-      customMarks: [
-        10, 30, 80, 91
-      ]
+      customMarks: []
     };
 
     var $window = $(window),
